8edddf02323362b0c7f45312fd0ce6a564ed55b5
kytz12 <mike.mxrisk@pm.me>
2026-01-26 15:07:42 -0600

    Update 02_compute_tpi.py

--- 02_compute_tpi.py
+++ 02_compute_tpi.py
@@ -1,1 +1,17 @@
-print('compute placeholder')
+import xarray as xr
+import numpy as np
+
+ds = xr.open_dataset("data/hrrr.grib2", engine="cfgrib")
+
+cape = ds["cape"]
+lcl = ds["lcl"]
+cin = ds["cin"]
+
+cape_n = np.clip(cape / 2000, 0, 1)
+lcl_n  = np.clip(1 - (lcl / 2000), 0, 1)
+cin_n  = np.clip(1 + (cin / 100), 0, 1)
+
+tpi = cape_n * lcl_n * cin_n
+
+out = xr.Dataset({"tpi": tpi})
+out.to_netcdf("data/tpi.nc")
