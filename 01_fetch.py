1fbaeda543de0e9e0a7bc39f869239609aa3ed8b
kytz12 <mike.mxrisk@pm.me>
2026-01-26 15:07:07 -0600

    Update 01_fetch.py

--- 01_fetch.py
+++ 01_fetch.py
@@ -1,1 +1,18 @@
-print('fetch placeholder')
+import requests
+from datetime import datetime
+import os
+
+base = "https://nomads.ncep.noaa.gov/pub/data/nccf/com/hrrr/prod"
+
+date = datetime.utcnow().strftime("%Y%m%d")
+hour = datetime.utcnow().strftime("%H")
+
+url = f"{base}/hrrr.{date}/conus/hrrr.t{hour}z.wrfsfcf01.grib2"
+
+os.makedirs("data", exist_ok=True)
+
+print("Downloading:", url)
+r = requests.get(url)
+r.raise_for_status()
+
+open("data/hrrr.grib2","wb").write(r.content)
