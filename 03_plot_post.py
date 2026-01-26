61b36a51bf18511311d7ccd76e6d790462e1a0c5
kytz12 <mike.mxrisk@pm.me>
2026-01-26 15:09:00 -0600

    Update 03_plot_post.py

--- 03_plot_post.py
+++ 03_plot_post.py
@@ -1,1 +1,18 @@
-print('plot placeholder')
+import xarray as xr
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import os
+
+ds = xr.open_dataset("data/tpi.nc")
+tpi = ds["tpi"]
+
+plt.figure(figsize=(10,6))
+ax = plt.axes(projection=ccrs.PlateCarree())
+ax.coastlines()
+ax.set_extent([-125, -66, 24, 50])
+
+tpi.plot(ax=ax, transform=ccrs.PlateCarree(), cmap="hot")
+
+os.makedirs("out", exist_ok=True)
+plt.savefig("out/tpi.png")
+plt.close()
